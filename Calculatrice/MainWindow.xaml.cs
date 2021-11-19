a37c756c39afa41db4193726600f8a8f5a553154
sha-zha <adeline.servan@gmail.com>
2021-11-19 09:36:47 +0400

    tentative realese

--- Calculatrice/MainWindow.xaml.cs
+++ Calculatrice/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Calculatrice
 {
@@ -117,8 +107,8 @@ namespace Calculatrice
                 //calcule
                 result = num1 + num2;
                 //resultat
-                input.Text = num1.ToString("F2") + "+" + num2.ToString("F2") + "="; 
-                input2.Text = result.ToString("F2");
+                input.Text = num1.ToString() + "+" + num2.ToString() + "="; 
+                input2.Text = result.ToString();
             }
             if (input.Text.Contains('-'))
             {
